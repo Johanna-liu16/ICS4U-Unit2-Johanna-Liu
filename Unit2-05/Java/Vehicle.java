c8c5a0f34dec93258329a21c05680b6512feb51a
Mr Coxall <mr.coxall@mths.ca>
2024-04-25 11:55:35 +0000

    init

---
+++ Unit2-05/Java/Vehicle.java
@@ -0,0 +1,19 @@
+/*
+ * This stack is for a vechicle.
+ *
+ * @author  Johanna
+ * @version 1.0
+ * @since   2024-04-24
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This is my stack.
+ */
+public class Vehicle {
+        private color: string
+        private licencePlate: string
+        private numberOfDoors: number
+        private maxSpeen: number
+        private theSpeed: number
